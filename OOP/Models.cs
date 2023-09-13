6c8c065a5eb5cce6a31f6d49f448f3ead081a745
Bishal Poudel <bishal@inficare.com.my>
2023-09-13 22:10:14 +0545

    Initial commit

---
+++ OOP/Models.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+   public abstract class BaseCustomer
+    {
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string BillAmount { get; set; } = string.Empty;
+        public string BillDate { get; set; } = string.Empty;
+        public abstract string ValidateModel();
+       
+
+
+    }
+    public class Customer : BaseCustomer
+    {
+        public override string ValidateModel() =>
+            string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(PhoneNumber) ||
+            string.IsNullOrEmpty(Address) || string.IsNullOrEmpty(BillAmount) ||
+            string.IsNullOrEmpty(BillDate)?"Validtion failed":"Validation passed";
+       
+
+    }
+    public class Lead : BaseCustomer
+    {
+        public override string ValidateModel() =>
+              string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(PhoneNumber) ||
+            string.IsNullOrEmpty(Address)?"Validaiton failed":"Validation passed";
+
+
+    }
+    public class Factory
+    {
+        private List<BaseCustomer> customers = new List<BaseCustomer>();
+        public BaseCustomer Create(int CustomerType)
+        {
+            if (customers.Count == 0) LoadCustomers();
+            return customers[CustomerType];
+        }
+        public void LoadCustomers()
+        {
+            customers.Add(new Lead());
+            customers.Add(new Customer());
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP;
+
+Console.WriteLine("Enter CustomerType");
+int customerType = Convert.ToInt32(Console.ReadLine());
+var userFactoryClass = new Factory().Create(customerType);
+Console.WriteLine("Enter Name");
+userFactoryClass.Name = Console.ReadLine();
+Console.WriteLine("Enter address");
+userFactoryClass.Address = Console.ReadLine();
+Console.WriteLine("Enter PhoneNumber");
+userFactoryClass.PhoneNumber = Console.ReadLine();
+Console.WriteLine("Enter BillAmount");
+userFactoryClass.BillAmount = Console.ReadLine();
+Console.WriteLine("Enter BillDate");
+userFactoryClass.BillDate = Console.ReadLine();
+Console.WriteLine(userFactoryClass.ValidateModel());
+Console.ReadKey();
+
+
